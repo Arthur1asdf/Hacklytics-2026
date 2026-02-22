4740796ea2474813f863e97b3995faa7cb780e05
Natalia Cano <140561361+ncano1025@users.noreply.github.com>
2026-02-21 22:47:40 -0500

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,104 +1,53 @@
 void setup() {
-  // setup port
   Serial.begin(9600);
+  pinMode(8, HIGH);
+  pinMode(9, HIGH);
+  pinMode(10, HIGH);
+  pinMode(11, HIGH);
+}
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-
-  
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
+void runStatus(char stat, int pin) {
+  if (stat == '0') { // GREEN
+    digitalWrite(pin, HIGH);
+    //delay(1000);
+  } else if (stat == '1') {
+    digitalWrite(pin, HIGH);
+    delay(500);
+    digitalWrite(pin, LOW);
+    delay(500);
+  } else if (stat == '2') {
+    digitalWrite(pin, HIGH);
+    delay(500);
+    digitalWrite(pin, LOW);
+    delay(500);
+  } else if (stat == '3') {
+    digitalWrite(pin, HIGH);
+    delay(500);
+    digitalWrite(pin, LOW);
+    delay(500);
+  }
 }
 
 void loop() {
-  // if (Serial.available()) {
-  //   char c = Serial.read();
-  //   if (c == 'left_arm') {
-  //     digitalWrite(8, HIGH);
-  //     delay(1000);
-  //     digitalWrite(8, LOW);
-  //     delay(1000);
-  //   }
-  //   if (c == 'right_arm'){
-  //     digitalWrite(9, HIGH);
-  //     delay(1000);
-  //     digitalWrite(9, LOW);
-  //     delay(1000);
-  //   }
-  //   if (c == 'left_leg'){
-  //     digitalWrite(10, HIGH);
-  //     delay(1000);
-  //     digitalWrite(10, LOW);
-  //     delay(1000);
-  //   }
-  //   if(c == 'right_leg'){
-  //     digitalWrite(11, HIGH);
-  //     delay(1000);
-  //     digitalWrite(11, LOW);
-  //     delay(1000);
-  //   }
-  //   if (c == 'chest'){
-  //     digitalWrite(8, HIGH);
-  //     digitalWrite(9, HIGH);
-  //     delay(1000);
-  //     digitalWrite(8, LOW);
-  //     digitalWrite(9, LOW);
-  //     delay(1000);
-  //   }
-  //   if(c == 'torso'){
-  //     digitalWrite(10, HIGH);
-  //     digitalWrite(11, HIGH);
-  //     delay(1000);
-  //     digitalWrite(10, LOW);
-  //     digitalWrite(11, LOW);
-  //     delay(1000);
-  //   }
-  // }
-  String c = "left_arm";
-    if (c == "left_arm") {
-      digitalWrite(8, HIGH);
-      delay(2000);
-      digitalWrite(8, LOW);
-      delay(1300);
+  if (Serial.available() >= 2) {
+    char limbChar = Serial.read();
+    char statusChar = Serial.read(); 
+
+    // Blink the LED to show we received something
+    switch (limbChar) {
+      case '0': // left arm
+        runStatus(statusChar, 8);
+        break;
+      case '1': // right arm
+        runStatus(statusChar, 9);
+        break;
+      case '2': // left leg
+        runStatus(statusChar, 10);
+        break;
+      case '3': // right leg
+        runStatus(statusChar, 11);
+        break;
     }
-    if (c == "right_arm"){
-      digitalWrite(9, HIGH);
-      delay(2000);
-      digitalWrite(9, LOW);
-      delay(1300);
-    }
-    if (c == "left_leg"){
-      digitalWrite(10, HIGH);
-      delay(2000);
-      digitalWrite(10, LOW);
-      delay(1300);
-    }
-    if(c == "right_leg"){
-      digitalWrite(11, HIGH);
-      delay(2000);
-      digitalWrite(11, LOW);
-      delay(1300);
-    }
-    if (c == "chest"){
-      digitalWrite(8, HIGH);
-      digitalWrite(9, HIGH);
-      delay(1000);
-      digitalWrite(8, LOW);
-      digitalWrite(9, LOW);
-      delay(1000);
-    }
-    if(c == "torso"){
-      digitalWrite(10, HIGH);
-      digitalWrite(11, HIGH);
-      delay(1000);
-      digitalWrite(10, LOW);
-      digitalWrite(11, LOW);
-      delay(1000);
-    }
+  }
+  digitalWrite(8, LOW);
 }
